a6955641319296c9a354e62ffd6858a29d46e052
pavankumar-1105 <87218290+pavankumar-1105@users.noreply.github.com>
2022-11-08 22:38:19 +0530

    Update MainController.java

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Controller;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainController {
