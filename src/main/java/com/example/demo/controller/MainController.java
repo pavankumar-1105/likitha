b2100b80e11fd9dba9309dafbe6aad1d55e6a774
pavankumar-1105 <87218290+pavankumar-1105@users.noreply.github.com>
2022-11-08 22:32:54 +0530

    Update MainController.java

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
     public String showLoginPage() {
-        return "index.html";
+        return "index";
     }
 
 }
